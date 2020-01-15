880531ae9e300954794a189b4e8f6990e11ece47
SuperY <yuanlinyue@gmail.com>
2020-01-15 13:09:59 +0800

    Support Carthage

---
+++ M80Attributed/M80Attributed/M80Attributed.h
@@ -0,0 +1,25 @@
+//
+//  M80Attributed.h
+//  M80Attributed
+//
+//  Created by Felix Yuan on 2020/1/15.
+//  Copyright © 2020 Ityly. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for M80Attributed.
+FOUNDATION_EXPORT double M80AttributedVersionNumber;
+
+//! Project version string for M80Attributed.
+FOUNDATION_EXPORT const unsigned char M80AttributedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <M80Attributed/PublicHeader.h>
+#import <M80Attributed/M80AttributedLabel.h>
+#import <M80Attributed/M80AttributedLabelURL.h>
+#import <M80Attributed/M80AttributedLabelDefines.h>
+#import <M80Attributed/M80AttributedLabelAttachment.h>
+
+
+
+
---
+++ M80Attributed/M80AttributedDemo/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  M80AttributedDemo
+//
+//  Created by Felix Yuan on 2020/1/15.
+//  Copyright © 2020 Ityly. All rights reserved.
+//
+
+import UIKit
+import M80Attributed
+
+class ViewController: UIViewController {
+
+    let label : M80AttributedLabel = M80AttributedLabel.init()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
